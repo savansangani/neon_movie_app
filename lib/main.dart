8307b0bcf8938986683633faee48f35479d214da
savansangani <savansangani1310@gmail.com>
2022-09-21 15:09:26 +0530

    feat: onboarding screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:neon_movie_app/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,10 +11,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Neon Movie App',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      theme: ThemeData(fontFamily: GoogleFonts.openSans().fontFamily),
+      home: const HomeScreen(),
     );
   }
 }
